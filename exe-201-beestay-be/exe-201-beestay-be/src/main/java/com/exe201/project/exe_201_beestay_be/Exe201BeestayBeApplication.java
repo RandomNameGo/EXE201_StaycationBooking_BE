7899c0b96c6e8b525b8104ad642e276f200d7a28
Admin <khangdoan20122004@gmail.com>
2025-05-21 16:23:47 +0700

    init

---
+++ exe-201-beestay-be/exe-201-beestay-be/src/main/java/com/exe201/project/exe_201_beestay_be/Exe201BeestayBeApplication.java
@@ -0,0 +1,13 @@
+package com.exe201.project.exe_201_beestay_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exe201BeestayBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exe201BeestayBeApplication.class, args);
+	}
+
+}
---
+++ exe-201-beestay-be/exe-201-beestay-be/src/test/java/com/exe201/project/exe_201_beestay_be/Exe201BeestayBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe201.project.exe_201_beestay_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exe201BeestayBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
