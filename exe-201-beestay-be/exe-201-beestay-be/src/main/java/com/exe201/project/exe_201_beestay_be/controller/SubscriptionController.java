831d6b83879c6f9022de74340c0643f88a8067f4
Admin <khangdoan20122004@gmail.com>
2025-06-28 13:50:30 +0700

    Minor change

---
+++ exe-201-beestay-be/exe-201-beestay-be/src/main/java/com/exe201/project/exe_201_beestay_be/controller/SubscriptionController.java
@@ -0,0 +1,25 @@
+package com.exe201.project.exe_201_beestay_be.controller;
+
+import com.exe201.project.exe_201_beestay_be.services.SubscriptionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/bee-stay/api/v1")
+public class SubscriptionController {
+
+    private final SubscriptionService subscriptionService;
+
+    @GetMapping("/subscription")
+    public ResponseEntity<?> getSubscriptions() {
+        try{
+            return ResponseEntity.ok(subscriptionService.getSubscriptions());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+}
